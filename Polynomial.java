4826555f019c1037f42992428ba0793b7065abed
Sadad Mahmood <sadadtur2007@gmail.com>
2026-06-02 00:01:41 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -14,28 +14,47 @@ public class Polynomial {
 
     public Polynomial add(Polynomial poly){
 
-        int length=Math.max(poly.coefficients.length, this.coefficients.length);
+        int texp = this.exponents[this.exponents.length - 1];
+        int pexp = poly.exponents[poly.exponents.length - 1];
 
-        double[] newList = new double[length];
+        int maxExp = Math.max(texp,pexp);
 
-        for (int i=0; i<length; i++){
-            double val1=0;
-            double val2=0;
+        double[] newList = new double[maxExp + 1];
 
-            if (i<this.coefficients.length){
-                val1=this.coefficients[i];
-            }
-
-            if (i<poly.coefficients.length){
-                val2=poly.coefficients[i];
-            }
-
-            newList[i]=val1+val2;
+        for(int i=0;i<this.exponents.length;i++){
+            int e = this.exponents[i];
+            newList[e]=newList[e]+this.coefficients[i];
         }
 
+        for(int i=0; i<poly.exponents.length; i++){
+            int e = poly.exponents[i];
+            newList[e]=newList[e]+poly.coefficients[i];
+        }
 
-        int [] placeholder = new int[10]; // so code does not break
-        return new Polynomial(newList, placeholder);
+        int count = 0;
+        for(int i=0; i<newList.length;i++){
+            if (newList[i]!=0){
+                count++;
+            }
+        }
+
+        if(count==0){
+            return new Polynomial();
+        }
+
+        double[] coeffs = new double[count];
+        int[] exps = new int[count];
+
+        int idx=0;
+        for(int i=0; i<newList.length; i++){
+            if(newList[i]!=0){
+                coeffs[idx]=newList[i];
+                exps[idx]=i;
+                idx++;
+            }
+        }
+        return new Polynomial(coeffs, exps);
+
     }
     
     public double evaluate(double x){
@@ -56,31 +75,44 @@ public class Polynomial {
 
     public Polynomial multiply(Polynomial poly){
 
-        int texp = 0;
-        for(int i=0;i<this.exponents.length;i++){
-            if(this.exponents[i]>texp){
-                texp=this.exponents[i];
-            }
-        }
-
-        int pexp = 0;
-        for(int i=0;i<poly.exponents.length;i++){
-            if(poly.exponents[i]>pexp){
-                pexp=poly.exponents[i];
-            }
-        }
+        int texp = this.exponents[this.exponents.length - 1];
+        int pexp = poly.exponents[poly.exponents.length - 1];
 
         int maxExp = texp + pexp;
 
         double[] newList = new double[maxExp + 1];
 
         for(int i=0;i<this.exponents.length;i++){
-            for(int j=0; i<poly.exponents.length;j++){
+            for(int j=0; j<poly.exponents.length;j++){
                 double c = this.coefficients[i]*poly.coefficients[j];
                 int e = this.exponents[i]+poly.exponents[j];
+
+                newList[e]=newList[e]+c;
             }
         }
 
-        return poly;
+        int count = 0;
+        for(int i=0; i<newList.length;i++){
+            if (newList[i]!=0){
+                count++;
+            }
+        }
+
+        if(count==0){
+            return new Polynomial();
+        }
+
+        double[] coeffs = new double[count];
+        int[] exps = new int[count];
+
+        int idx=0;
+        for(int i=0; i<newList.length; i++){
+            if(newList[i]!=0){
+                coeffs[idx]=newList[i];
+                exps[idx]=i;
+                idx++;
+            }
+        }
+        return new Polynomial(coeffs, exps);
     }
 }
